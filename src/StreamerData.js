50613c61785171036cdee3ea4e13938e6796dc4d
Furkan Ergüldürenler <furkanerguldurenler@hotmail.com>
2024-05-15 20:48:49 +0300

    expanding database

--- src/StreamerData.js
+++ src/StreamerData.js
@@ -16,8 +16,37 @@ import oguzhanUgur from "./assets/streamer-pictures/oguzhanugur.jpg";
 import barisOzcan from "./assets/streamer-pictures/barisozcan.jpg";
 import danlaBilic from "./assets/streamer-pictures/danlabilic.jpg";
 import merveOzkaynak from "./assets/streamer-pictures/merveozkaynak.jpg";
-
 import reynmen from "./assets/streamer-pictures/reynmen.jpg";
+import bixi from "./assets/streamer-pictures/bixi.jpg";
+import testoTaylan from "./assets/streamer-pictures/testotaylan.jpg";
+import tunaTavus from "./assets/streamer-pictures/tunatavus.jpg";
+import alperRende from "./assets/streamer-pictures/alperrende.jpg";
+import mesutcanTomay from "./assets/streamer-pictures/mesutcantomay.jpg";
+import aliBicim from "./assets/streamer-pictures/alibicim.jpg";
+import efeUygac from "./assets/streamer-pictures/efeuygac.jpg";
+import betul from "./assets/streamer-pictures/betul.jpg";
+import mertcanBahar from "./assets/streamer-pictures/mertcanbahar.jpg";
+import kerimcanDurmaz from "./assets/streamer-pictures/kerimcan.jpg";
+import lucifer from "./assets/streamer-pictures/lucifer.jpg";
+import porcay from "./assets/streamer-pictures/porcay.jpg";
+import gorilFaruk from "./assets/streamer-pictures/gorilfaruk.jpg";
+import eserYenenler from "./assets/streamer-pictures/eseryenenler.jpg";
+import erayCanOzkenar from "./assets/streamer-pictures/eray.jpg";
+import swaggyBark from "./assets/streamer-pictures/swaggy.jpg";
+import sametYilmaz from "./assets/streamer-pictures/sametortaam.jpg";
+import cagriErgün from "./assets/streamer-pictures/hype.jpg";
+import gugucan from "./assets/streamer-pictures/gugucan.jpg";
+import meryemCan from "./assets/streamer-pictures/meryem.jpg";
+import miafitz from "./assets/streamer-pictures/miafitz.jpg";
+import muratAbiGf from "./assets/streamer-pictures/muratabigf.jpg";
+import yarenAlaca from "./assets/streamer-pictures/yarenalaca.jpg";
+import cemreSolmaz from "./assets/streamer-pictures/cemresolmaz.jpg";
+import canSungur from "./assets/streamer-pictures/cansungur.jpg";
+import tolunayOren from "./assets/streamer-pictures/tolunayoren.jpg";
+import efeAydal from "./assets/streamer-pictures/efeaydal.jpg";
+import silaErtas from "./assets/streamer-pictures/silaertas.jpg";
+import ekinKollama from "./assets/streamer-pictures/taa.jpg";
+
 export const streamerList = [
   {
     img: elraenn,
@@ -185,8 +214,270 @@ export const streamerList = [
     img: merveOzkaynak,
     name: "Merve Özkaynak",
     gender: "Kadın",
+    nickName: null,
     followerCount: 2200000,
     birthYear: 1987,
     category: ["Moda", "Güzellik"],
+  },
+  {
+    img: bixi,
+    name: "Bahadır Telci",
+    gender: "Erkek",
+    nickName: "PurpleBixi",
+    followerCount: 473000,
+    birthYear: 1998,
+    category: ["Siyaset", "Sohbet"],
+  },
+  {
+    img: testoTaylan,
+    name: "Taylan Özgüç Danyıldız",
+    gender: "Erkek",
+    nickName: "Testo Taylan",
+    followerCount: 645000,
+    birthYear: 1997,
+    category: ["Fitness", "Sohbet"],
+  },
+  {
+    img: tunaTavus,
+    name: "Tuna Tavus",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 2000000,
+    birthYear: 1999,
+    category: ["Fitness", "Yemek"],
+  },
+  {
+    img: alperRende,
+    name: "Alper Rende",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 5000000,
+    birthYear: 1995,
+    category: ["Eğlence"],
+  },
+  {
+    img: mesutcanTomay,
+    name: "Mesutcan Tomay",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 2210000,
+    birthYear: 1999,
+    category: ["Eğlence"],
+  },
+  {
+    img: aliBicim,
+    name: "Ali Biçim",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 2170000,
+    birthYear: 1987,
+    category: ["Eğlence"],
+  },
+  {
+    img: efeUygac,
+    name: "Efe Uygaç",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 617000,
+    birthYear: 1995,
+    category: ["Eğlence", "Sohbet"],
+  },
+  {
+    img: betul,
+    name: "Betül Çakmak",
+    gender: "Kadın",
+    nickName: null,
+    followerCount: 2340000,
+    birthYear: 1996,
+    category: ["Moda", "Güzellik"],
+  },
+  {
+    img: mertcanBahar,
+    name: "Mertcan Bahar",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 1820000,
+    birthYear: 1998,
+    category: ["Eğlence"],
+  },
+  {
+    img: kerimcanDurmaz,
+    name: "Kerimcan Durmaz",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 4000000,
+    birthYear: 1994,
+    category: ["Eğlence", "Şarkı"],
+  },
+  {
+    img: lucifer,
+    name: "Kaan Alkan",
+    gender: "Erkek",
+    nickName: "Lucifer Michaelson",
+    followerCount: 2000000,
+    birthYear: 1990,
+    category: ["Eğlence", "Sohbet"],
+  },
+  {
+    img: porcay,
+    name: "Ömer Aslan",
+    gender: "Erkek",
+    nickName: "Porçay",
+    followerCount: 1170000,
+    birthYear: 1996,
+    category: ["Eğlence", "Sohbet"],
+  },
+  {
+    img: gorilFaruk,
+    name: "Ömer Faruk Müftüoğlu",
+    gender: "Erkek",
+    nickName: "Goril Faruk",
+    followerCount: 2250000,
+    birthYear: 1996,
+    category: ["Eğlence", "Spor"],
+  },
+  {
+    img: eserYenenler,
+    name: "Eser Yenerler",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 1170000,
+    birthYear: 1984,
+    category: ["Eğlence", "Talk Show"],
+  },
+  {
+    img: erayCanOzkenar,
+    name: "Eray Can Özkenar",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 1100000,
+    birthYear: 2001,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: swaggyBark,
+    name: "Alpcan Alpar",
+    gender: "Erkek",
+    nickName: "Swaggy Bark",
+    followerCount: 126400,
+    birthYear: 1998,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: sametYilmaz,
+    name: "Samet Yılmaz",
+    gender: "Erkek",
+    nickName: "Ortaam Samet",
+    followerCount: 25000,
+    birthYear: 1990,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: cagriErgün,
+    name: "Çağrı Ergün",
+    gender: "Erkek",
+    nickName: "Hype",
+    followerCount: 792000,
+    birthYear: 1990,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: gugucan,
+    name: "Mazhar Kantaş",
+    gender: "Erkek",
+    nickName: "Gugucan",
+    followerCount: 50000,
+    birthYear: 1994,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: meryemCan,
+    name: "Meryem Can",
+    gender: "Kadın",
+    nickName: null,
+    followerCount: 5000000,
+    birthYear: 1995,
+    category: ["Eğlence", "Sohbet"],
+  },
+  {
+    img: miafitz,
+    name: "Gözde Demiral",
+    gender: "Kadın",
+    nickName: "Miafitz",
+    followerCount: 764000,
+    birthYear: 1998,
+    category: ["Oyun", "Sohbet"],
+  },
+  {
+    img: muratAbiGf,
+    name: "Murat Engin Ekin",
+    gender: "Erkek",
+    nickName: "Murat abi gf",
+    followerCount: 2730000,
+    birthYear: 1997,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: yarenAlaca,
+    name: "Yaren Alaca",
+    gender: "Kadın",
+    nickName: null,
+    followerCount: 5700000,
+    birthYear: 2003,
+    category: ["Eğlence", "Makyaj"],
+  },
+  {
+    img: cemreSolmaz,
+    name: "Cemre Solmaz",
+    gender: "Kadın",
+    nickName: null,
+    followerCount: 10600000,
+    birthYear: 1999,
+    category: ["Eğlence", "Makyaj"],
+  },
+  {
+    img: canSungur,
+    name: "Can Sungur",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 316000,
+    birthYear: 1986,
+    category: ["Oyun", "Sohbet", "Eğlence"],
+  },
+  {
+    img: tolunayOren,
+    name: "Tolunay Ören",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 1230000,
+    birthYear: 1997,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: efeAydal,
+    name: "Efe Aydal",
+    gender: "Erkek",
+    nickName: null,
+    followerCount: 605000,
+    birthYear: 1981,
+    category: ["Sohbet", "Bilim", "Eğlence"],
+  },
+  {
+    img: silaErtas,
+    name: "Sıla Ertaş",
+    gender: "Kadın",
+    nickName: null,
+    followerCount: 1600000,
+    birthYear: 2003,
+    category: ["Oyun", "Eğlence"],
+  },
+  {
+    img: ekinKollama,
+    name: "Ekin Kollama",
+    gender: "Erkek",
+    nickName: "Taa",
+    followerCount: 1100000,
+    birthYear: 1992,
+    category: ["Oyun", "Eğlence"],
   },
 ];
